556f2c485ae140f975d3f8f0e5004b428a91e839
Azuama Odinaka Maximus <azuama69@gmail.com>
2022-09-16 12:41:22 +0100

    some changes in the css file, login page and signup page

--- login.php
+++ login.php
@@ -10,7 +10,7 @@
     <div class='logDiv'>
     <h1>Log In</h1>
     <p>No account <a href="signup.php">Register here</a></p>
-    <form action="includes/login-inc.php" method='POST'>
+    <form action="" method='POST'>
         <input id='loginInput' type="text" name='username' placeholder= 'username'>
         <input id='loginInput' type="password" name='password' placeholder='password'>
         <button type='submit' name="submit" id='sub'>LOGIN</button>
--- signup.php
+++ signup.php
@@ -10,7 +10,7 @@
     <div class='logDiv'>
     <h1>Register</h1>
     <p>Already have an account <a href="login.php">Login!</a></p>
-    <form action="includes/register-inc.php" method="POST">
+    <form action="" method="POST">
         <input id='loginInput' type="text" name='username' placeholder= 'username'>
         <input id='loginInput'  type="password" name='password' placeholder='password' min = '8'>
         <input id='loginInput'  type="password" name='confrimPassword' placeholder='confirm password' min = '8'>
