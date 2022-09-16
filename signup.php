f7f40b1c9156c0436806b4e382799aa88699b8cb
Azuama Odinaka Maximus <azuama69@gmail.com>
2022-09-16 14:31:43 +0100

    projectWork change from html file to php file

--- signup.php
+++ signup.php
@@ -1,3 +1,26 @@
+<?php 
+    require_once "includes/database.php";
+
+    if(isset($_POST['submit'])) {
+
+        $user_name = $_POST['username'];
+        $password = $_POST['password'];
+        $confimr_password = $_POST['confirmPassword'];
+
+        if(!empty($password)) {
+            if($password === $confimr_password){
+                $query = "INSERT INTO users(username, password) VALUES('$user_name', '$password');";
+                $q = $conn->query($query);
+                header('Location:../projectWork.php?success=registractionsuccessful');
+            }else{
+                echo 'password does not match';
+            }
+            
+            // $query = "INSERT INTO users(username, password) VALUES(?, ?);";
+           
+        }
+    }
+?>
 
 <html lang="en">
 <head>
@@ -11,10 +34,10 @@
     <div class='logDiv'>
     <h1>Register</h1>
     <p>Already have an account <a href="login.php">Login!</a></p>
-    <form action="" method="POST">
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
         <input id='loginInput' type="text" name='username' placeholder= 'username'>
         <input id='loginInput'  type="password" name='password' placeholder='password' min = '8'>
-        <input id='loginInput'  type="password" name='confrimPassword' placeholder='confirm password' min = '8'>
+        <input id='loginInput'  type="password" name='confirmPassword' placeholder='confirm password' min = '8'>
         <button type='submit' name='submit' id='sub'>REGISTER</button>
     </form>
 </div>
